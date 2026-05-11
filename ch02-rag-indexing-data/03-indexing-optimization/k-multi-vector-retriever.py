27d38a74dac81531cdfcb3bb18c90f377d019ab7
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2026-05-10 19:06:12 -0500

    feat:[RAG Patterns] MultiVectorRetriever

--- ch02-rag-indexing-data/03-indexing-optimization/k-multi-vector-retriever.py
+++ ch02-rag-indexing-data/03-indexing-optimization/k-multi-vector-retriever.py
@@ -1,5 +1,8 @@
 from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import (
+    Language,
+    RecursiveCharacterTextSplitter
+)
 from langchain_ollama import OllamaEmbeddings
 from langchain_postgres.vectorstores import PGVector
 from langchain_core.output_parsers import StrOutputParser
@@ -17,14 +20,15 @@ collection_name = "summaries"
 embeddings_model = OllamaEmbeddings(model="embeddinggemma")
 
 # Load the document
-loader = TextLoader("../test.txt", encoding="utf-8")
+loader = TextLoader("../design-patterns.txt", encoding="utf-8")
 docs = loader.load()
 print("Length of loaded docs: ", len(docs[0].page_content))
 
 # Split the document
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.MARKDOWN,
+    chunk_size=700,
+    chunk_overlap=80
 )
 chunks = splitter.split_documents(docs)
 
@@ -32,30 +36,51 @@ chunks = splitter.split_documents(docs)
 prompt_text = "Summarize the following document:\n\n{doc}"
 
 prompt = ChatPromptTemplate.from_template(prompt_text)
-llm = ChatOllama(temperature=0, model="granite4.1:3b")
+llm = ChatOllama(temperature=0, model="deepseek-r1:1.5b")
 
 summarize_chain = {
     "doc": lambda x : x.page_content
 } | prompt | llm | StrOutputParser()
 
 # batch the chain across the chunks
-summaries = summarize_chain.batch(chunks, {"max_concurrency": 5})
+summaries = summarize_chain.batch(chunks, {"max_concurrency": 1})
 
 # The vectorstore to use to index the child chunks
 vectorstore = PGVector(
     embeddings=embeddings_model,
     collection_name=collection_name,
     connection=connection,
-    use_jsonb=True,
+    use_jsonb=True
 )
 
 # The storage layer for the parent documents
 store = InMemoryStore()
 id_key = "doc_id"
 
-# indexing the summaries in our vector store, whilst retaining the original documents in our document store:
+# indexing the summaries in our vector store, whilst retaining the original
+# documents in our document store
 retriever = MultiVectorRetriever(
     vectorstore=vectorstore,
     docstore=store,
     id_key=id_key,
 )
+
+# Changed from summaries to chunks since we need same length as docs
+doc_ids = [str(uuid.uuid4()) for _ in chunks]
+
+# Each summary is linked to the original document by the doc_id
+summary_docs = [
+    Document(page_content=s, metadata={id_key: doc_ids[i]})
+    for i, s in enumerate(summaries)
+]
+
+# Add the document summaries to the vector store for similarity search
+retriever.vectorstore.add_documents(summary_docs)
+
+# Store the original documents in the document store, linked to their summaries via doc_ids
+# This allows us to first search summaries efficiently, then fetch the full docs when needed
+retriever.docstore.mset(list(zip(doc_ids, chunks)))
+
+retrieved_docs = retriever.invoke("factory design pattern")
+
+print(retrieved_docs)
